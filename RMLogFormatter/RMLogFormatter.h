124a4dca101df69224b7861ef57ba0256f999cf0
Ryan Maloney <ryan_maloney@me.com>
2014-09-06 16:31:29 -0700

    Forgot to include CocoaLumberjack dependency.

--- RMLogFormatter/RMLogFormatter.h
+++ RMLogFormatter/RMLogFormatter.h
@@ -24,6 +24,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CocoaLumberjack/CocoaLumberjack.h>
 
 // TODO: Add customization flags to conditionally include addtional information
 @interface RMLogFormatter : NSObject <DDLogFormatter>
