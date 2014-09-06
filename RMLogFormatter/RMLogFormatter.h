05247e2dcbd64d44f39d5aaf3c8c2aacef560891
Ryan Maloney <ryan_maloney@me.com>
2014-09-06 14:31:37 -0700

    Initial class commit.

---
+++ RMLogFormatter/RMLogFormatter.h
@@ -0,0 +1,13 @@
+//
+//  RMLogFormatter.h
+//  
+//
+//  Created by Ryan Maloney on 9/6/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RMLogFormatter : NSObject
+
+@end
---
+++ RMLogFormatter/RMLogFormatter.m
@@ -0,0 +1,13 @@
+//
+//  RMLogFormatter.m
+//  
+//
+//  Created by Ryan Maloney on 9/6/14.
+//
+//
+
+#import "RMLogFormatter.h"
+
+@implementation RMLogFormatter
+
+@end
