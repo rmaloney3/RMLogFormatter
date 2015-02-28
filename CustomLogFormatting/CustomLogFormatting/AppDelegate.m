516ee5258d7a81fe7517dcbf2520b4f377c49105
Ryan Maloney <ryan_maloney@me.com>
2015-02-28 11:07:41 -0800

    Added log formatter init to sample app

--- CustomLogFormatting/CustomLogFormatting/AppDelegate.m
+++ CustomLogFormatting/CustomLogFormatting/AppDelegate.m
@@ -7,6 +7,10 @@
 //
 
 #import "AppDelegate.h"
+#import <CocoaLumberjack/CocoaLumberjack.h>
+#import <RMLogFormatter/RMLogFormatter.h>
+
+static const DDLogLevel ddLogLevel = DDLogLevelVerbose;
 
 @interface AppDelegate ()
 
@@ -17,7 +21,26 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    RMLogFormatter *logFormatter = [[RMLogFormatter alloc] init];
     
+    [[DDTTYLogger sharedInstance] setLogFormatter:logFormatter];
+    
+    [[DDTTYLogger sharedInstance] setColorsEnabled:YES];
+    [[DDTTYLogger sharedInstance] setForegroundColor:[UIColor colorWithRed:0.7294 green:0.2078 blue:0.1843 alpha:1.0] backgroundColor:nil forFlag:DDLogFlagError];
+    [[DDTTYLogger sharedInstance] setForegroundColor:[UIColor colorWithRed:0.9686 green:0.6627 blue:0.0902 alpha:1.0] backgroundColor:nil forFlag:DDLogFlagWarning];
+    [[DDTTYLogger sharedInstance] setForegroundColor:[UIColor colorWithRed:0.2510 green:0.4941 blue:0.7216 alpha:1.0] backgroundColor:nil forFlag:DDLogFlagInfo];
+    [[DDTTYLogger sharedInstance] setForegroundColor:[UIColor colorWithRed:0.3765 green:0.2588 blue:0.6314 alpha:1.0] backgroundColor:nil forFlag:DDLogFlagDebug];
+    [[DDTTYLogger sharedInstance] setForegroundColor:[UIColor colorWithRed:0.9255 green:0.9412 blue:0.9451 alpha:1.0] backgroundColor:nil forFlag:DDLogFlagVerbose];
+    
+    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+    
+    DDLogVerbose(@"Verbose");
+    DDLogDebug(@"Debug");
+    DDLogInfo(@"Info");
+    DDLogWarn(@"Warn");
+    DDLogError(@"Error");
+    
+    DDLogVerbose(@"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
     
     return YES;
 }
